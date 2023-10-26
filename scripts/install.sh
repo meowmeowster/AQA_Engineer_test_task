0017d37bc9a195a01af3d25a3f1ffbb0bf3c560d
Ivan Petrov <kote27@yandex.ru>
2023-10-27 01:49:23 +0300

    flow experiments

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,6 +7,7 @@ pwd
 ls -la
 pip install --upgrade pip
 pip install -r ./requirements.txt
+sudo apt update
 sudo apt install openjdk-8-jdk
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 echo $JAVA_HOME
